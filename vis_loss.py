d97de72a4ff0bc3c6717eaf75a8af4c1ae5220b4
William Kent <williamkent@Williams-MacBook-Pro-6.local>
2025-03-31 20:32:19 -0400

    Added command line execution for vis_loss.py

--- vis_loss.py
+++ vis_loss.py
@@ -11,6 +11,15 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 import math
+import argparse
+
+def parseargs():
+    p = argparse.ArgumentParser(description="Plots training loss from ANN training")
+    p.add_argument('version',type=int,help="training version")
+    
+    args = p.parse_args()
+    
+    return args
 
 def average_values_by_key(list1, list2):
     # Create lists to store the keys and averages
@@ -41,97 +50,56 @@ def average_values_by_key(list1, list2):
 
 # Example usage:
 
+if __name__ == "__main__":
 
+    # Parse command line arguments
+    args = parseargs()
 
-DIR_PATH = './resultsANN/lightning_logs/version_17'
+    DIR_PATH = './resultsANN/lightning_logs/version_'+str(args.version)
 
-data = pd.read_csv(os.path.join(DIR_PATH,'metrics.csv'),header=0)
+    data = pd.read_csv(os.path.join(DIR_PATH,'metrics.csv'),header=0)
 
-epochs = []
-train_loss = []
-val_loss = []
-train_loss_step = []
-epoch_step = []
+    epochs = []
+    train_loss = []
+    val_loss = []
+    train_loss_step = []
+    epoch_step = []
 
-for i in range(len(data)):
-    if math.isnan(data['train_loss'][i]) != True:
-        epochs.append(data['epoch'][i])
-        train_loss.append(data['train_loss'][i])
-    if math.isnan(data['val_loss'][i]) != True:
-        val_loss.append(data['val_loss'][i])
-    #if math.isnan(data['train_loss_step'][i]) != True:
-     #   train_loss_step.append(data['train_loss_step'][i])
-      #  epoch_step.append(data['epoch'][i])
+    for i in range(len(data)):
+        if math.isnan(data['train_loss'][i]) != True:
+            epochs.append(data['epoch'][i])
+            train_loss.append(data['train_loss'][i])
+        if math.isnan(data['val_loss'][i]) != True:
+            val_loss.append(data['val_loss'][i])
+        #if math.isnan(data['train_loss_step'][i]) != True:
+         #   train_loss_step.append(data['train_loss_step'][i])
+          #  epoch_step.append(data['epoch'][i])
+            
+
+    epochs_2,train_loss_2 = average_values_by_key(train_loss_step,epoch_step)
         
-
-epochs_2,train_loss_2 = average_values_by_key(train_loss_step,epoch_step)
-    
-    
-start_index = 2
-
-
-plt.close('all')
-plt.figure(figsize=(12,5))
-
-plt.subplot(1,2,1)
-plt.plot(epochs[start_index:],train_loss[start_index:],color='blue',label='Train')
-plt.plot(epochs[start_index:],val_loss[start_index:],color='green',label='Validation')
-plt.xlabel('Epoch',fontsize=12)
-plt.ylabel('MSE',fontsize=12)
-plt.legend()
-
-plt.subplot(1,2,2)
-plt.plot(epochs[start_index:],train_loss[start_index:],color='blue',label='Train')
-plt.plot(epochs[start_index:],val_loss[start_index:],color='green',label='Validation')
-plt.xlabel('Epoch',fontsize=12)
-plt.ylabel('MSE',fontsize=12)
-plt.yscale('log')
-plt.legend()
-# plt.ylim(0,0.0005)
-plt.tight_layout()
-
-plt.savefig(os.path.join(DIR_PATH,'loss.png'),dpi=300)
-
-# plt.close('all')
-# plt.figure(figsize=(5,5))
-
-# plt.subplot(1,1,1)
-# plt.plot(epochs_2[start_index:],train_loss_2[start_index:],color='blue',label='Padded Train')
-# plt.plot(epochs[start_index:],val_loss[start_index:],'b--',label='Padded Validation')
-
-
-# DIR_PATH = './results/lightning_logs/version_72'
-
-# data = pd.read_csv(os.path.join(DIR_PATH,'metrics.csv'),header=0)
-
-# epochs = []
-# train_loss = []
-# val_loss = []
-# train_loss_step = []
-# epoch_step = []
-
-# for i in range(len(data)):
-#     if math.isnan(data['train_loss_epoch'][i]) != True:
-#         epochs.append(data['epoch'][i])
-#         train_loss.append(data['train_loss_epoch'][i])
-#     if math.isnan(data['val_loss_epoch'][i]) != True:
-#         val_loss.append(data['val_loss_epoch'][i])
-#     if math.isnan(data['train_loss_step'][i]) != True:
-#         train_loss_step.append(data['train_loss_step'][i])
-#         epoch_step.append(data['epoch'][i])
         
+    start_index = 2
 
-# epochs_2,train_loss_2 = average_values_by_key(train_loss_step,epoch_step)
 
-# plt.plot(epochs_2[start_index:],train_loss_2[start_index:],'g-',label='No Padding Train')
-# plt.plot(epochs[start_index:],val_loss[start_index:],'g--',label='No Padding Validation')
+    plt.close('all')
+    plt.figure(figsize=(12,5))
 
-# plt.xlabel('Epoch',fontsize=12)
-# plt.ylabel('MSE',fontsize=12)
-# plt.yscale('log')
-# plt.legend()
-# # plt.ylim(0,0.0005)
-# plt.tight_layout()
+    plt.subplot(1,2,1)
+    plt.plot(epochs[start_index:],train_loss[start_index:],color='blue',label='Train')
+    plt.plot(epochs[start_index:],val_loss[start_index:],color='green',label='Validation')
+    plt.xlabel('Epoch',fontsize=12)
+    plt.ylabel('MSE',fontsize=12)
+    plt.legend()
 
-# # plt.savefig(os.path.join(DIR_PATH,'loss_comp_47.png'),dpi=300)
+    plt.subplot(1,2,2)
+    plt.plot(epochs[start_index:],train_loss[start_index:],color='blue',label='Train')
+    plt.plot(epochs[start_index:],val_loss[start_index:],color='green',label='Validation')
+    plt.xlabel('Epoch',fontsize=12)
+    plt.ylabel('MSE',fontsize=12)
+    plt.yscale('log')
+    plt.legend()
+    # plt.ylim(0,0.0005)
+    plt.tight_layout()
 
+    plt.savefig(os.path.join(DIR_PATH,'loss.png'),dpi=300)
